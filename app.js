cbfabb7e1ab35e79addb397d8641108c9b732886
parbhat-dhanuk <162868710+parbhat-dhanuk@users.noreply.github.com>
2024-06-23 13:53:16 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+let userScore=0;
+let compScore=0;
+
+const choices =document.querySelectorAll(".choice");
+
+const msg=document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+
+const compScorePara = document.querySelector("#comp-score");
+
+const genCompChoice=()=>{
+
+    const options = ["rock","paper","scissors"];
+    const randIdx = Math.floor(Math.random()*3);
+    return options[randIdx];
+}
+
+const drawGame=()=>{
+
+
+    msg.innerText="Draw";
+    msg.style.background="#00B7C6";
+
+}
+const showWinner=(userWin, userChoice,compChoice)=>{
+    if(userWin){
+      
+        userScore++;
+        userScorePara.innerText= userScore;
+        msg.innerText=`You Win  Your ${userChoice} beats ${compChoice}`;
+        msg.style.background="green";
+
+    } else{
+
+        compScore++;
+        compScorePara.innerText= compScore;
+        msg.innerText=`You Lose ${compChoice} beats your ${userChoice} `;
+        msg.style.background="red";
+    }
+}
+
+const playGame=(userChoice)=>{
+    console.log("user choice =",userChoice)
+
+   //Generate computer choice
+   const compChoice = genCompChoice();
+   console.log("comp choice =",compChoice)
+
+   //Draw game
+   if(userChoice==compChoice){
+    drawGame();
+   }
+   else{
+
+
+    let userWin = true;
+    if(userChoice=="rock"){
+        //scissors,paper
+        compChoice=="paper"?false:true;
+
+    }
+
+
+    else if(userChoice=="paper"){
+        //rock, scissors
+        compChoice=="scissors"?false:true;
+
+    }
+    else{
+
+
+        //rock,paper
+        userWin=compChoice=="rock"?false:true;
+
+
+    }
+
+    showWinner(userWin,userChoice,compChoice);
+
+   }
+}
+
+        choices.forEach((choice)=>{
+
+        choice.addEventListener("click", ()=>{
+
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+
+    })
+})
+
